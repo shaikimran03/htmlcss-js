6c198ffefb04f0fef036ecbc38df1c3265c7a793
shaik imran <imranshaik2393is@gmail.com>
2024-11-01 22:32:02 +0530

    commit

---
+++ switches.js
@@ -0,0 +1,82 @@
+
+/* no switches
+
+let day=7;
+
+
+if(day==1){
+    console.log(`monday`);
+}
+else if(day==2){
+    console.log(`tuesday`);
+}
+else if(day==3){
+    console.log(`wednesday`);
+}
+ else if(day==4){
+    console.log(`thursday`);
+}
+else if(day==5){
+    console.log(`friday`);
+}
+ else if(day==6){
+    console.log(`saturday`);
+}
+else if(day==7){
+    console.log(`sunday`);
+}
+else{
+    console.log(`${day} is not a day`);
+}
+
+*/
+
+
+
+/*let day= 2;
+switch(day){
+    case 1:
+        console.log(`its monday`);
+        break;
+
+    case 2:
+        console.log(`its tues`);
+        break;
+    case 3:
+        console.log(`its wedday`);
+        break;        
+    case 4:
+        console.log(`its thursday`);
+        break;
+    
+    case 5:
+        console.log(`its friday`);
+        break;
+    case 6:
+        console.log(`its satday`);
+       break;    
+    case 7:
+        console.log(`sunday`);
+        break;     
+    default:
+        console.log(`${day} is not a day`); 
+                 
+
+}
+        */
+
+//evaluating grades
+
+let testscore=92;
+let lettergrade;
+
+switch(true)
+
+{
+    case testscore>=90:
+    lettergrade="a";
+    break;
+}
+
+console.log(lettergrade);
+
---
+++ ternaryoperator.js
@@ -0,0 +1,21 @@
+//condition ? codeiftrue : codeiffalse;
+
+
+/*let age=21;
+ let message = age>=18 ? 'youare an adult ' : `minor`;
+console.log(message);*/
+
+
+//let time = 16;
+//let greeting =time <12 ? "good morning" : "good night";
+//console.log(greeting);
+
+///let isStudent=true;
+//let message= isStudent ? "yes" : `no`;
+//console.log(message);
+
+
+let amount=125;
+let discount= amount>=100 ? 10 :0;
+console.log(discount);
+console.log(`your total is $${amount- amount*(discount/100)}`);
