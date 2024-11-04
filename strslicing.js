97187f74253666175c808fe3ffc6da1bc01c209d
shaik imran <imranshaik2393is@gmail.com>
2024-11-04 17:03:31 +0530

    commit

---
+++ strslicing.js
@@ -0,0 +1,8 @@
+// creating a substring from a portion of another string 
+
+// string.slice(start, end)
+
+const fullName="im ran";
+
+let firstName = fullName.slice(0, 2);
+console.log(firstName);
