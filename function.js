a1e474e51a13377635f6342ea44a44782bd63dc5
shaik imran <imranshaik2393is@gmail.com>
2024-11-27 15:42:21 +0530

    commit

---
+++ function.js
@@ -0,0 +1,8 @@
+// a section of reusable cpde 
+// declare once use it many times 
+// call to execute it 
+
+function hbd(){
+    console.log(`hbd`);
+}
+hbd(); // to call a function add fname and ();
