f474d4c232a072e38fd74cd3733d26897c180650
shaik imran <imranshaik2393is@gmail.com>
2024-11-27 15:54:56 +0530

    commit

--- function.js
+++ function.js
@@ -2,7 +2,28 @@
 // declare once use it many times 
 // call to execute it 
 
-function hbd(){
-    console.log(`hbd`);
+function happtBirthday(username,age){
+    console.log(`hbd ${username} ${age}`);
 }
-hbd(); // to call a function add fname and ();
+happtBirthday(`imran`,25); // to call a function add fname and ();
+
+function add(x ,y ){
+    let result=x+y;
+    return result;
+}
+// add(2,3) in this no output would be shown 
+// so we write a variable and add output statement 
+ let answer=add(2,3);
+ // or console.log(add(2,3))
+ console.log(answer);
+
+
+ function isvalid(email){
+    if(email.includes(`@`)){// built in method for checking a character in a string 'includes' 
+        return true;
+    }else{
+        return false;
+    }
+
+ }
+ console.log(isvalid(`imran@gamil.com`))
